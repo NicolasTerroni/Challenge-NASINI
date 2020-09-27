43cdcf14bddecef2349f6fc7fe8aaad6b7be4c61
Nicolas Terroni <nsterroni@gmail.com>
2020-09-27 16:10:35 -0300

    login function

--- init.py
+++ init.py
@@ -6,9 +6,29 @@ import pyRofex
 # Credentials
 import credentials
 
+def login(user, password, account):
+    """Tries to login."""
+
+    print("Logging in reMarkets.")
+    try:
+        pyRofex.initialize(user, password, account, pyRofex.Environment.REMARKET)
+    except:
+        print("Failed to log in.")
+        return False
+    print(f"Logged in as: {credentials.user}")
+    return True
+
+
 
 def run():
-    pass
+    user = credentials.user
+    password = credentials.password
+    account = credentials.account
+
+    login(user, password, account)
+        #symbol = input("Input symbol: ")
+
+
 
 
 if __name__ == "__main__":
